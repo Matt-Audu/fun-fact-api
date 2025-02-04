6b0cc678cd6ef41ae578cd25900e198d67360c63
Matt-Audu <simplymailmatthew7@gmail.com>
2025-02-04 10:03:00 +0100

    update

--- main.py
+++ main.py
@@ -83,7 +83,7 @@ async def classify_number(number: str = Query(..., description="The number to cl
         num = int(number)
     except ValueError:
         raise HTTPException(
-            status_code=200,
+            status_code=400,
             detail={"number": number, "error": True}
         )
     
